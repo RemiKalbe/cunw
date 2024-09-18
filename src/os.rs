6cd4c4360db6f4a50190c775bccebc665d5033f1
Rémi Kalbe <remi.kalbe@pm.me>
2024-09-18 11:46:22 -0500

    Update os.rs
    May fix windows build issue

--- src/os.rs
+++ src/os.rs
@@ -1,10 +1,13 @@
-use miette::Result;
+use std::os::windows::fs::MetadataExt;
+
 use walkdir::DirEntry;
+
+use crate::error::{CunwError, Result};
 
 // Windows-specific constant used to check if a file is hidden.
 const FILE_ATTRIBUTE_HIDDEN: u32 = 0x00000002;
 
 #[cfg(windows)]
 pub fn is_hidden_dir_entry(file: &DirEntry) -> Result<bool> {
-    Ok(file.metadata()?.file_attributes() & FILE_ATTRIBUTE_HIDDEN != 0)
+    Ok(file.metadata().map_err(|err| CunwError::from(err.into()).with_file(file.into_path()))?.file_attributes() & FILE_ATTRIBUTE_HIDDEN != 0)
 }
